1ee16d7edf1265e60a323c4fa3de352d2720c8af
严俊东 <1172482914@qq.com>
2019-03-07 10:13:35 +0800

    【拣货任务没有出库单号显示】修复

--- src/views/outwarehousing/components/config.js
+++ src/views/outwarehousing/components/config.js
@@ -84,6 +84,7 @@ export const pickingtaskConfig=[
   { label:'拣货单号',prop:'orderCode'},
   { label:'拣货总数',prop:'realPickQty'},
   { label:'应拣货总量',prop:'pickQty'},
+  { label:'出库单号',prop:'outOrderCode'},
   { label:'状态',prop:'orderStatus',type:'orderStatus',useLocalEnum:true},
   { label:'创建时间',prop:'gmtCreate',type:'time'},
   { label:'操作',useDom:true,width:300},
