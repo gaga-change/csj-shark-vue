633875f2dcc954b39bb97a04a29b63c887a10450
严俊东 <1172482914@qq.com>
2019-08-08 15:06:09 +0800

    接口error处理修正

--- src/api/http.js
+++ src/api/http.js
@@ -35,7 +35,7 @@ newAxios.interceptors.response.use(function (response) {
   return data
 }, function (error) {
   let message = error.message || err.errorMsg || ''
-  if (message = 'timeout of 1500ms exceeded') message = '请求超时，请稍后再试！'
+  if (message === 'timeout of 1500ms exceeded') message = '请求超时，请稍后再试！'
   Notification({
     title: '错误信息',
     message: message,
