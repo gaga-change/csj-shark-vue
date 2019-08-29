688dec4b1831dbebb51075c7894f5e832f42e3b6
严俊东 <1172482914@qq.com>
2019-08-29 11:29:28 +0800

    update

--- src/template/dialogForm01.vue
+++ src/template/dialogForm01.vue
@@ -77,6 +77,16 @@
             >
             </el-switch>
           </el-form-item>
+          <!-- 单选 -->
+          <el-form-item
+            label="特殊资源"
+            prop="resource"
+          >
+            <el-radio-group v-model="formData.resource">
+              <el-radio label="线上品牌商赞助"></el-radio>
+              <el-radio label="线下场地免费"></el-radio>
+            </el-radio-group>
+          </el-form-item>
         </el-form>
       </div>
       <!-- <el-alert
