bfa514a8f37d6a0255dfbad971129a0361008e3e
严俊东 <1172482914@qq.com>
2019-09-27 16:48:56 +0800

    移库记录创建时间字段修正

--- src/views/inventoryManage/moveLibraryRecordList.vue
+++ src/views/inventoryManage/moveLibraryRecordList.vue
@@ -24,7 +24,7 @@ const tableConfig = [
   { label: '移入库位', prop: 'tWarehouseSpaceCode' },
   { label: '移库数量', prop: 'moveQty' },
   { label: '创建人', prop: 'createrName' },
-  { label: '创建时间', prop: 'createTimeArea', type: 'time' },
+  { label: '创建时间', prop: 'gmtCreate', type: 'time' },
 ]
 const searchConfig = [
   { label: '商品编码 ', prop: 'skuCode' },
