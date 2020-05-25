bdcc001785b44ec46a2fc31d9a26e6da53740b6d
严俊东 <1172482914@qq.com>
2020-05-25 15:47:38 +0800

    收货明细列表界面调整：加入商品状态

--- src/views/inwarehousing/goodsDetailList.vue
+++ src/views/inwarehousing/goodsDetailList.vue
@@ -57,6 +57,7 @@ const tableConfig = [
   { label: '批次', prop: 'batchNo', type: 'batchNoPopover', width: 110 },
   { label: '容器', prop: 'trayCode' },
   { label: '实际收货量', prop: 'receiveQty' },
+  { label: '商品状态', prop: 'AA', type: 'enum', enum: 'productStatusEnum' },
   { label: '已上架数量', prop: 'realInQty' },
   { label: '上架状态', prop: 'isPut', type: 'enum', enum: 'isPutEnum' },
   { label: '收货时间', prop: 'gmtCreate', type: 'time', width: 140 },
